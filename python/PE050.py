f47155b9eb57584f98ddc9a10333e1c1e95988ca
Long Qian <qianlongzju@gmail.com>
2011-09-01 07:59:29 +0800

    fix problem 2.1 in TODO

--- python/PE050.py
+++ python/PE050.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 from PE007 import isPrime
+from PE010 import getPrimeList
 
 def main():
-    t = []
-    for i in range(2, 1000000):
-        if isPrime(i):
-            t.append(i)
+    t = getPrimeList(1000000)
+    totallen = len(t)
     length = 0
-    for i in range(len(t)):
+    for i in range(totallen):
+        if i+length >= totallen:
+            break
         Sum = sum(t[i:i+length])
-        for j in range(i+length, len(t)):
+        for j in range(i+length, totallen):
             Sum += t[j]
             if Sum > 1000000:
                 break
-            if isPrime(Sum) and j-i+1 > length:
+            if j-i+1 > length and isPrime(Sum):
                 length = j-i+1
                 Max = Sum
     print Max
