88ddb6a575d314dcaae8153fc1917e1060bd1fb4
Long Qian <qianlongzju@gmail.com>
2011-09-11 10:35:27 +0800

    python solution to problem 57

---
+++ python/PE057.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from fractions import gcd
+
+def main():
+    num = 1
+    den = 2
+    count = 0
+    for i in range(1000):
+        if len(str(num + den)) > len(str(den)):
+            count += 1
+        tmp = den
+        den = 2 * den + num
+        num = tmp
+        tmp = gcd(num, den)
+        den /= tmp
+        num /= tmp 
+    print count
+
+if __name__ == '__main__':
+    main()
