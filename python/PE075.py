ae2476ea9b84810583ece9a6681804c81015d0bf
Long Qian <qianlongzju@gmail.com>
2011-09-12 13:13:43 +0800

    python solution to problem 75 but runs exceeds 1m
    this issue is added to TODO file

---
+++ python/PE075.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from PE009 import getPythagoreanTriplet
+
+def isTrue(s):
+    return sum(1 for i in getPythagoreanTriplet(s)) == 1
+
+def main():
+    print sum(1 for i in range(12, 1500001, 2) if isTrue(i))
+
+if __name__ == '__main__':
+    main()
