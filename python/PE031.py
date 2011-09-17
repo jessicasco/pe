001a39007e91ef6ff4b4b54cef4ed8c53558a546
Long Qian <qianlongzju@gmail.com>
2011-09-17 10:05:07 +0800

    fix PE031.py

--- python/PE031.py
+++ python/PE031.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
 def dynamic():
-    """Dynamic Programming"""
+    """
+    Dynamic Programming
+    """
     coins = [1, 2, 5, 10, 20, 50, 100, 200]
     ways = [0] * (200 + 1)
     ways[0] = 1
     for coin in coins:
         for i in range(coin, 200 + 1):
             ways[i] += ways[i-coin]
-        print ways
     print ways[200]
 
-def main():
-    """recursion"""
+def recursive():
+    """
+    Recursive
+    """
     L = [200, 100, 50, 20, 10, 5, 2, 1]
 
     def f(money, maxcoin):
@@ -22,11 +25,15 @@ def main():
         for i in range(maxcoin, len(L)):
             if money - L[i] == 0:
                 s += 1
-            if money - L[i] > 0:
+            elif money - L[i] > 0:
                 s += f(money - L[i], i)
         return s
     
     print f(200, 0)
+
+def main():
+    dynamic()
+    # recursive()
 
 if __name__ == '__main__':
     main()
