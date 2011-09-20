948c7aa010aca1141db8cb5f030df0581870998f
Long Qian <qianlongzju@gmail.com>
2011-09-21 00:02:35 +0800

    fix tabs of PE005.py

--- python/PE005.py
+++ python/PE005.py
@@ -2,13 +2,13 @@
 import math
 
 def main():
-	"""Only need to calculate the max powers of the primes which are under the target."""
-	primeList = [2, 3, 5, 7, 11, 13, 17, 19] 
-	res = 1
-	for prime in primeList:
-		maxPower = int(math.floor(math.log(20, prime)))
-		res *= pow(prime, maxPower)
-	print res
+    """Only need to calculate the max powers of the primes which are under the target."""
+    primeList = [2, 3, 5, 7, 11, 13, 17, 19] 
+    res = 1
+    for prime in primeList:
+        maxPower = int(math.floor(math.log(20, prime)))
+        res *= pow(prime, maxPower)
+    print res
 
 if __name__ == '__main__':
     main()
