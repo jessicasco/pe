cbb9e9f94dc47dfe53df0648b59ebbf5bb2122b1
Long Qian <qianlongzju@gmail.com>
2011-10-29 22:31:03 +0800

    use pell equation to solve PE100

--- python/PE100.py
+++ python/PE100.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
-import math
-def main():
-    n = 35 + 85
-    sqrt2 = 1.0 / math.sqrt(2)
+from fractions import gcd
+def solve_pell():
+    num = 1
+    den = 2
     while True:
-        x = int(sqrt2 * n) + 1
-        if 2*x*(x-1) == n*(n-1):
-            result = x
-            if n > 10**12: 
-                break
-            n = int(5.8284 * n)
-        n += 1
-    print result
+        if (num+den)*(num+den) - 2*den*den == 1:
+            return num+den, den
+        tmp = den
+        den = 2 * den + num
+        num = tmp
+        tmp = gcd(num, den)
+        den /= tmp
+        num /= tmp 
+
+def main():
+    a, b = solve_pell()
+    total = 120
+    tt = 2*120-1
+    blue = 85
+    bb = 2*85-1
+    while total <= 1000000000000:
+        temp = a*tt + 2*b*bb
+        bb = b*tt + a*bb
+        tt = temp
+        total = (tt+1)/2
+        blue = (bb+1)/2
+    print blue
 
 if __name__ == '__main__':
     main()
